f3110ce890077f686a9ca1d8adbdb599ca3d3a61
Alejandro GX <alex@flyingpigs.es>
2018-04-12 12:29:52 +0200

    pst

--- post-receive.php
+++ post-receive.php
@@ -1,8 +1,14 @@
 <?php
 
   $payload = json_decode($_POST['payload'], true);
-  $branch = substr($payload['ref'], strrpos($payload['ref'], '/'));
-  echo $branch;
-  exec('git pull origin master');
-  
+  $branch = substr($payload['ref'], strrpos($payload['ref'], '/') + 1);
+
+  if ($branch == 'master'){
+    exec('git pull origin master');
+    echo "Push en master detectado. Pull realizado con exito.";
+  }
+  else {
+    echo "Push en " . $branch . " detectado. No se realiza acción.";
+  }
+
 ?>
